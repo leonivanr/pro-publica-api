7183f0881eea6fb782e7c74a991d128c245e445b
Leon <leonivanrodrigo@gmail.com>
2019-04-09 19:16:16 -0300

    Finalizado tarea 6.

--- js/main.js
+++ js/main.js
@@ -6,23 +6,24 @@ createSenateTable();
 function createSenateTable() {
   // Busco el elemento que tiene el id "senate-data" (tabla) y le pido que lo aloje en la variable "elSenateTable".
   var elSenateTable = document.getElementById('senate-data');
-  // Copio el contenido el array "members" en la variable tableElement.
+  // Copio el contenido del array "members" en la variable tableElement.
   tableElement = addTableToHTML(dataSenate.results[0].members);
-  //Para despues cambiar el contenido de la tabla en el HTML
+  //Actualizo el contenido de la tabla en el HTML
   elSenateTable.innerHTML = tableElement;
 
 }
 
 function addTableToHTML(membersArray) {
- //
-  var elementHtml = '<thead class="thead-light"><tr><th scope="col"> Full Name </th><th scope="col"> Party </th><th scope="col"> State </th><th scope="col"> Seniority </th><th scope="col"> Percentage of votes with party </th></tr></thead>';
+  // Creo una variable que contenga las etiquetas con los titulos de cada columna de la tabla.
+  var elementHtml = '<thead class="thead-light"><tr><th> Full Name </th><th> Party Affiliation</th><th> State </th><th> Seniority </th><th> Percentage of votes with party </th></tr></thead>';
+  // Le agrego la etiqueta <tbody> al cuerpo de la tabla.
   elementHtml += '<tbody>';
-//
+  // Recorro el array "members", y por cada item, agrego una fila (<tr>) y las columnas que se necesitan.
   membersArray.forEach(function(member) {
     elementHtml += '<tr>';
-    if (member.middle_name === null) {
+    if (member.middle_name === null) { // Si no tiene segundo nombre, solo agrega nombre y apellido.
       elementHtml += '<td><a href="' + member.url + '">' + member.first_name + ' ' + member.last_name + '</td>';
-    } else {
+    } else { // De lo contrario, nombre, segundo nombre y apellido.
       elementHtml += '<td><a href="' + member.url + '">' + member.first_name + ' ' + member.middle_name + ' ' + member.last_name + '</a></td>';
     }
     elementHtml += '<td>' + member.party + '</td>';
