f05d67a85d7c425fd51b941890ae6a3eb4c4ca7e
Leon <leonivanrodrigo@gmail.com>
2019-04-21 16:24:14 -0300

    Finish task 2.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ verifyPage();
 
 // Verifica si es Senadores o Congreso, en base a eso, llama a la función createTable con sus respectivos datos.
 function verifyPage() {
-
-  if (document.getElementById("senate-table")) {
+  var idTable = document.getElementById("senate-table");
+  if (idTable) {
     createTable("senate-table", filterTable(dataSenate.results[0].members));
   } else {
     createTable("house-table", filterTable(dataHouse.results[0].members));
@@ -21,23 +21,22 @@ function createTable(id, data) {
 }
 // Recorre el array y crea el head y el body de la tabla.
 function addTableToHTML(membersArray) {
-
   // Creo una variable que contenga las etiquetas con los titulos de cada columna de la tabla.
-  var elementHtml = '<thead class="thead-light"><tr><th> Name </th><th> Party Affiliation</th><th> State </th><th> Seniority </th><th> Percentage of votes with party </th></tr></thead>';
+  var elementHtml = '<thead class="thead-light"><tr><th> Name </th><th> Party Affiliation</th><th> State </th><th> Seniority </th><th> Votes with party </th></tr></thead>';
   // Le agrego la etiqueta <tbody> al cuerpo de la tabla.
   elementHtml += '<tbody>';
   // Recorro el array "members", y por cada item, agrego una fila (<tr>) y las columnas que se necesitan.
   membersArray.forEach(function (member) {
     elementHtml += '<tr>';
     if (member.middle_name === null) { // Si no tiene segundo nombre, solo agrega nombre y apellido.
-      elementHtml += '<td><a href="' + member.url + '">' + member.first_name + ' ' + member.last_name + '</td>';
+      elementHtml += '<td><a href="' + member.url + '">' + member.first_name + ' ' + member.last_name + '</a></td>';
     } else { // De lo contrario, nombre, segundo nombre y apellido.
       elementHtml += '<td><a href="' + member.url + '">' + member.first_name + ' ' + member.middle_name + ' ' + member.last_name + '</a></td>';
     }
     elementHtml += '<td>' + member.party + '</td>';
     elementHtml += '<td>' + member.state + '</td>';
     elementHtml += '<td>' + member.seniority + '</td>';
-    elementHtml += '<td>' + member.votes_with_party_pct + ' % </td>';
+    elementHtml += '<td id="percent">' + member.votes_with_party_pct + ' % </td>';
     elementHtml += '</tr>';
   });
   elementHtml += '</tbody>';
@@ -55,22 +54,22 @@ function filterTable(members) {
     memberState = members[i].state;
     // Si el checkbox da verdadero, comprueba si hay algún estado seleccionado y la compara, después copia el elemento
     // en el array "filters".
-    if ((selectedState === "All" || selectedState === memberState) && (document.getElementById("check-independent").checked === true)) {
-      if (members[i].party === "I") {
-        filters.push(members[i]);
-      }
-    }
+
     if ((selectedState === "All" || selectedState === memberState) && (document.getElementById("check-republican").checked === true)) {
-      if (members[i].party === "R") {
+      if (members[i].party == "R") {
         filters.push(members[i]);
       }
     }
     if ((selectedState === "All" || selectedState === memberState) && (document.getElementById("check-democrat").checked === true)) {
-      if (members[i].party === "D") {
+      if (members[i].party == "D") {
         filters.push(members[i]);
       }
     }
-
+    if ((selectedState === "All" || selectedState === memberState) && (document.getElementById("check-independent").checked === true)) {
+      if (members[i].party == "I") {
+        filters.push(members[i]);
+      }
+    }
   }
   return filters;
 
